16cf7ea738290eb0bb0aac652fbbd78ecb5bbe3e
hassan490 <82362736+hassan490@users.noreply.github.com>
2026-02-22 16:31:51 +0400

    Enhance ALUOutput with comparison case
    Added case for comparison in ALUOutput method.

--- ALU.java
+++ ALU.java
@@ -7,13 +7,10 @@ public class ALU {
             case 1: result = readData1 - readData2; break;
             case 2: result = readData1 & readData2; break;
             case 3: result = readData1 | readData2; break;
+            case 4: result = readData1 - readData2; if(result < 0){result = 0;} else{ result = 1;} break;
             default:
                 result = 0;
         }
         return result;
     }
-    
-    
 }
-
-
