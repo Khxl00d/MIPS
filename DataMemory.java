26ac1e9d3c309148f6a054b3d46748f8787ee088
Khalid Eyad <119678212+Khxl00d@users.noreply.github.com>
2026-02-22 13:51:54 +0400

    Update DataMemory.java

--- DataMemory.java
+++ DataMemory.java
@@ -1,3 +1,24 @@
-class DataMemory {
+import java.util.HashMap;
 
+public class DataMemory {
+    private HashMap<Integer, Integer> memory;
+
+    public DataMemory() {
+        this.memory = new HashMap<>();
+    }
+    
+    int readData(int address, boolean readMem) {
+        if (readMem) {
+            return memory.getOrDefault(address, 0);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    void writeData(int address, int value, boolean writeMem) {
+        if (writeMem) {
+            memory.put(address, value);
+        }
+    }
 }
