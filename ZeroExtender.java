c7cdf276665ecb2da9f1f20c4a1e4a179a0303cf
hassan490 <82362736+hassan490@users.noreply.github.com>
2026-02-23 18:05:32 +0400

    Update ZeroExtender.java

--- ZeroExtender.java
+++ ZeroExtender.java
@@ -1,6 +1,15 @@
-public class ZeroExtender{
+public class ZeroExtender {
+
     public String getZeroExtender(int imm){
-        return (imm<<16) >>> 16;
+
+        String binary = Integer.toBinaryString(imm);
+
+        while(binary.length() < 16){
+            binary = "0" + binary;
+        }
+
+        String result = "0000000000000000" + binary;
+
+        return result;
     }
 }
-
