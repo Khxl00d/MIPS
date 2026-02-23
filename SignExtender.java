8b242c2b683689af3340416c112b35137b3f27fd
RavenKenji99 <sanamasbout99@gmail.com>
2026-02-23 17:34:28 +0400

    SignExtender.java

--- SignExtender.java
+++ SignExtender.java
@@ -1,10 +1,7 @@
 public class SignExtender {
 
-    public int extendTo32(int imm) {
-        if ((imm & 0x8000) != 0) { // check sign bit
-            return imm | 0xFFFF0000; // fill upper bits with 1s
-        } else {
-            return imm & 0x0000FFFF; // keep positive value
-        }
-    }
+   public int signExtend(int imm) {
+    return (imm << 16) >> 16;
 }
+}
+
