116dc95a1a1a7761c464ebe550bcfeaf85ab0f75
hassan490 <82362736+hassan490@users.noreply.github.com>
2026-02-23 17:52:31 +0400

    Update ZeroExtender.java

--- ZeroExtender.java
+++ ZeroExtender.java
@@ -1,5 +1,6 @@
 public class ZeroExtender{
-    public String getZeroExtender(int instruction){
-        return "0000000000000000" + instruction;
+    public String getZeroExtender(int imm){
+        return (imm<<16) >>> 16;
     }
 }
+
