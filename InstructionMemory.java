983d7bb5c9aa19bd409d59f8ba7c92789aed3c34
Khalid Eyad <119678212+Khxl00d@users.noreply.github.com>
2026-02-23 12:16:26 +0400

    Update InstructionMemory.java

---
+++ InstructionMemory.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class InstructionMemory {
+
+    private ArrayList<Instruction> insMem;
+
+    public InstructionMemory() {
+        this.insMem = new ArrayList<>();
+    }
+
+    public Instruction getInstruction(ProgramCounter PC) {
+        int index = PC.getPC()/4;
+        if (index >= 0 && index < insMem.size()) {
+            return insMem.get(index);
+        }
+        else {
+            return null;
+        }
+    }
+    public void storeInstruction(Instruction inst) {
+        insMem.add(inst);
+    }
+}
