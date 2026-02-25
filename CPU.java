7955aadd4c6f29ae2e11ebc92bfe4037a859f230
Khalid Eyad <119678212+Khxl00d@users.noreply.github.com>
2026-02-25 20:05:29 +0400

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -1,6 +1,14 @@
 public class CPU {
-
+    ProgramCounter PC;
     public CPU() {
-
+        this.PC = new ProgramCounter(0);
+    }
+    //to be continued...
+    public void loadWord(InstructionMemory loadMemory) {
+        DataMemory LDM = new DataMemory();
+        Instruction loadInst = loadMemory.getInstruction(PC);
+        int Rt = loadInst.getRt();
+        int address= loadInst.getRs()+loadInst.getImmediate();
+        Rt = LDM.readData(address,0);
     }
 }
