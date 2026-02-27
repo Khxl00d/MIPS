f16405256e87583ef93c1057f1f9af02b1b98022
Khalid Eyad <119678212+Khxl00d@users.noreply.github.com>
2026-02-27 15:37:48 +0400

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -17,7 +17,8 @@ public class CPU {
     }
 
     public void loadWord(InstructionMemory loadMemory) {
-        
+
+        PC.incrementPC();
         instruction = loadMemory.getInstruction(PC);
          
         int Rt = instruction.getRt();
@@ -27,11 +28,11 @@ public class CPU {
         int value = Memory.readData(address,controlUnit.controlSignals(instruction.getOpcode())[3]);
 
         registers.writeRegister(Rt, value, controlUnit.controlSignals(instruction.getOpcode())[8]);
-        PC.incrementPC();
     }
 
     public void storeWord(InstructionMemory storeMemory) {
         
+        PC.incrementPC();
         instruction = storeMemory.getInstruction(PC);
          
         int Rt = instruction.getRt();
@@ -41,11 +42,11 @@ public class CPU {
         int value = registers.readRegister(Rt);
         
         Memory.writeData(address,value,controlUnit.controlSignals(instruction.getOpcode())[6]);
-        PC.incrementPC();
     }
 
     public void shiftLeftLogical(InstructionMemory shiftRd) {
 
+        PC.incrementPC();
         instruction = shiftRd.getInstruction(PC);
 
         int Rt = instruction.getRt();
@@ -54,7 +55,6 @@ public class CPU {
         int value = registers.readRegister(Rt)*2<<Shamt;
 
         registers.writeRegister(Rd,value,controlUnit.controlSignals(instruction.getOpcode())[8]);
-        PC.incrementPC();
     }
     
 }
