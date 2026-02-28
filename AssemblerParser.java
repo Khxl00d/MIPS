dc38da12019c896e58f36fd622d8f1bb83580b87
Khalid Eyad <119678212+Khxl00d@users.noreply.github.com>
2026-02-28 13:16:59 +0400

    Update AssemblerParser.java

--- AssemblerParser.java
+++ AssemblerParser.java
@@ -68,6 +68,7 @@ public class AssemblerParser {
         identifyImmediate(arrInstruction);
         identifytarget(arrInstruction);
         identifyShamt(arrInstruction);
+        parseLabel();
         storeInstruction();
     }
 
