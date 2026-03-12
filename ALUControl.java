992e7d43d2be9b1311b30610df6727de775c455c
hassan490 <82362736+hassan490@users.noreply.github.com>
2026-03-12 12:57:31 +0400

    Enhance getALUControl to handle I-type operations

--- ALUControl.java
+++ ALUControl.java
@@ -1,27 +1,37 @@
 public class ALUControl {
 
-    public int getALUControl(int ALUop, int function) {
+    public int getALUControl(int ALUop, int function, int opCode) {
 
         //R-type
         if (ALUop == 2) {
-            if (function == 32) {
+            if (function == 32) { //add
                 return 0;
             }
-            else if (function == 34) {
+            else if (function == 34) { //sub
                 return 1;
             }
-            else if (function == 36) {
+            else if (function == 36) { //and
                 return 2;
             }
-            else if (function == 37) {
+            else if (function == 37) { //or
                 return 3;
             }
-            else if (function == 42) {
+            else if (function == 42) { //slt
                 return 4;
             }
-            else if (function == 39){
+            else if (function == 39){ //nor
                 return 5;
             }
+        }
+        //I-type
+        else if (opCode == 8){ //addi
+            return 0;
+        }
+        else if (opCode == 12){ //andi
+            return 2;
+        }
+        else if (opCode == 13){ //ori
+            return 3;
         }
         //sw lw
         else if (ALUop == 0) {
